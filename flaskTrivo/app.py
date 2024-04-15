8a5d425cba8990d1e27423f64b825bff5eccf684
oscauebarbosa <124931002+oscauebarbosa@users.noreply.github.com>
2024-04-15 17:25:10 -0300

    Add files via upload

--- flaskTrivo/app.py
+++ flaskTrivo/app.py
@@ -3,8 +3,22 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def inicio():
+    return render_template('Inicio.html')
+
+@app.route('/geral')
+def geral():
+    return render_template('Geral.html')
+
+@app.route('/addreceitas')
+def addreceitas():
+    return render_template('AddReceitas.html')
+
+@app.route('/adddespesas')
+def adddespesas():
+    return render_template('AddDespesas.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
